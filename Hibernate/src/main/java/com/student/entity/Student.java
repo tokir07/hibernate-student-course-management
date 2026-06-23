3ec90df913d9ca779571eaf9c4badda84b4ddcec
tokir07 <tokirkhan00291@gmail.com>
2026-06-23 15:30:28 +0530

    Implement Student entity and sace application

---
+++ Hibernate/src/main/java/com/student/entity/Student.java
@@ -0,0 +1,52 @@
+package com.student.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer studentId ;
+	
+	private String name ;
+	private String email ;
+	
+	public Student() {
+		
+	}
+
+	public Student(String name, String email) {
+		this.name = name;
+		this.email = email;
+	}
+
+	public Integer getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(Integer studentId) {
+		this.studentId = studentId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+}
